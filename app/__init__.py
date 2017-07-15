5b4d341e85d3a0fd28a4c8785e4ad65c55bbf689
ndass6 <ND@USComputers.net>
2017-07-15 04:51:06 -0700

    Added both API endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -34,12 +34,53 @@ def closeDB(error):
   if hasattr(g, 'mysql_db'):
     g.mysql_db.close()
 
-# Secret key is necessary for creating user sessions within the app
-app.secret_key = 'D8K27qBS8{8*sYVU>3DA530!0469x{'
-
 @app.route('/')
 def hello_world():
   return render_template('index.html')
+
+@app.route('/upc')
+def process_upc():
+  # Retrieve args
+  upc = request.args.get('num')
+  user_id = request.args.get('user_id')
+
+  # Retrieve food info
+  getCursor().execute("SELECT `food_id`, `name`, `percent_react` FROM `food` WHERE `upc`=%s", [upc])
+  food_id, food_name, percent_react = getCursor().fetchone()
+
+  # Retrieve user symtom info for people that had a reaction
+  getCursor().execute("SELECT `user_id`, `reaction` FROM `food_symptoms` WHERE `food_id`=%s AND NOT `reaction`='None'", [food_id])
+  user_ids, reactions = zip(*getCursor().fetchall())
+
+  if hasattr(g, 'mysql_db'):
+    g.mysql_db.close()
+
+  return jsonify(food_name=food_name, percent_react=percent_react, user_ids=user_ids, reactions=reactions)
+
+@app.route('/react')
+def process_react():
+  # Retrieve args
+  try:
+    user_id = request.args.get('user_id')
+    reaction = request.args.get('reaction')
+    food_name = request.args.get('food_name')
+
+    getCursor().execute("SELECT `food_id` FROM `food` WHERE `name`=%s", [food_name])
+    food_id = getCursor().fetchone()[0]
+
+    getCursor().execute("INSERT INTO `food_symptoms`(`user_id`,`reaction`,`food_id`) VALUES (%s,%s,%s)",
+      [user_id, reaction, food_id])
+    getDB().commit()
+
+    if hasattr(g, 'mysql_db'):
+      g.mysql_db.close()
+
+    return "True"
+  except:
+    if hasattr(g, 'mysql_db'):
+      g.mysql_db.close()
+
+    return "False"
 
 if __name__ == '__main__':
   app.run()
