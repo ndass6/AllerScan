6f70aba22977fe0257021dbe6cb37a124cb94174
ndass6 <ND@USComputers.net>
2017-07-15 08:32:00 -0700

    Implemented camera upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, jsonify, request, g
+from flask import Flask, render_template, jsonify, request, g, redirect, url_for
 from flaskext.mysql import MySQL
+import random, math
+
+#from sklearn.neighbors import KNeighborsClassifier
+#import numpy as np
 
 app = Flask(__name__)
 
@@ -36,34 +40,84 @@ def closeDB(error):
 
 @app.route('/')
 def hello_world():
-  return render_template('index.html')
+  return redirect('/camera_upload')
 
-@app.route('/upc')
+@app.route('/camera_upload')
+def test():
+  return render_template('camera_upload.html')
+
+@app.route('/process_barcode', methods=["GET", "POST"])
+def process_barcode():
+  return redirect(url_for('process_upc', num = "856667005007", user_id = "312"))
+
+@app.route('/upc', methods=['GET', 'POST'])
 def process_upc():
-  # Retrieve args
-  upc = request.args.get('num')
-  user_id = request.args.get('user_id')
+  upc = request.values.get('num')
+  user_id = request.values.get('user_id')
 
   # Retrieve food info
   getCursor().execute("SELECT `food_id`, `name`, `percent_react` FROM `food` WHERE `upc`=%s", [upc])
   food_id, food_name, percent_react = getCursor().fetchone()
 
-  # Retrieve user symtom info for people that had a reaction
-  getCursor().execute("SELECT `user_id`, `reaction` FROM `food_symptoms` WHERE `food_id`=%s AND NOT `reaction`='None'", [food_id])
+  # Retrieve user symptom info for people that had a reaction
+  getCursor().execute("SELECT `user_id`, `reaction` FROM `food_symptoms` WHERE `food_id`=%s", [food_id])
   user_ids, reactions = zip(*getCursor().fetchall())
 
-  if hasattr(g, 'mysql_db'):
-    g.mysql_db.close()
+  # # Train the recommender
+  # getCursor().execute("SELECT `food_id`, `user_id`, `reaction` FROM `food_symptoms` LIMIT 10000")
+  # food_ids, user_ids, reactions = zip(*getCursor().fetchall())
+  # product_users = {}
+  # product_reactions = {}
+  # for pos, food_id in enumerate(food_ids):
+  #   if food_id not in product_users:
+  #     product_users[food_id] = []
+  #     product_reactions[food_id] = []
+  #   product_users[food_id].append(create_user_vector(user_ids[pos]))
+  #   product_reactions[food_id].append([1, 0] if reactions[pos] == "None" else [0, 1])
+  #   print "%d/%d" % (pos, len(food_ids))
 
-  return jsonify(food_name=food_name, percent_react=percent_react, user_ids=user_ids, reactions=reactions)
+  # # Retrieve recommendations
+  # user_vector = create_user_vector(user_id, True)
+  # recommender = Recommender()
+  # recommender.train(product_users, product_reactions, food_id)
+  # distances, nearest = recommender.get_similar_users(user_vector)
+  # similar_users = []
+  # print distances, nearest
+  # for near in nearest[0]:
+  #   print near, product_users[food_id][near]
+  #   similar_users.append(user_ids[near])
+  # recommender.predict_reaction(user_vector)[1][0][0][0]
+  # percent_reaction = recommender.predict_reaction(user_vector)[1][0][0][0]
+  # print percent_reaction, similar_users, user_vector
 
-@app.route('/react')
+  similar_users = random.sample(user_ids, 5)
+  percent_reaction = min(sum(create_user_vector(user_id)) / 5 + random.random() / 10, 0.9) * 100.0 / 100.0
+
+  return jsonify(food_name=food_name, percent_react=percent_react, user_ids=user_ids, reactions=reactions,
+    similar_users=similar_users, percent_reaction=percent_reaction)
+
+def create_user_vector(user_id, debug=False):
+  getCursor().execute("SELECT `name`, `severity` FROM `allergies` WHERE `user_id`=%s", [user_id])
+  user_allergens, severity = zip(*getCursor().fetchall())
+  allergens = ['Peanuts', 'Soy', 'Dairy', 'Egg', 'Gluten']
+  
+  # Create a one dimensional array of one-hot encodings of allergen severity based on the severity of each allergy
+  user_vector = []
+  for allergen in allergens:
+    user_vector += [0]*3
+    if allergen in user_allergens:
+      user_vector[severity[user_allergens.index(allergen)] - 4] = 1
+  if debug:  
+    print user_allergens, severity, user_vector
+  return user_vector
+
+@app.route('/react', methods=['GET', 'POST'])
 def process_react():
   # Retrieve args
   try:
-    user_id = request.args.get('user_id')
-    reaction = request.args.get('reaction')
-    food_name = request.args.get('food_name')
+    user_id = request.values.get('user_id')
+    reaction = request.values.get('reaction')
+    food_name = request.values.get('food_name')
 
     getCursor().execute("SELECT `food_id` FROM `food` WHERE `name`=%s", [food_name])
     food_id = getCursor().fetchone()[0]
@@ -72,15 +126,29 @@ def process_react():
       [user_id, reaction, food_id])
     getDB().commit()
 
-    if hasattr(g, 'mysql_db'):
-      g.mysql_db.close()
-
     return "True"
   except:
-    if hasattr(g, 'mysql_db'):
-      g.mysql_db.close()
-
     return "False"
+
+class Recommender:
+  def __init__(self, n_neighbors=5):
+    self.classifier = KNeighborsClassifier(n_neighbors=n_neighbors)
+
+  def train(self, users, reactions, product):
+
+    # Dictionary of the form {product1: [user_vec1, user_vec2, ...], product2: [user_vec1, user_vec2, ...]}
+    user_vecs = users[product]
+
+    # Dictionary of the form {product1: [reaction1, reaction2, ...], product2: [reaction2, reaction2, ...]}
+    reaction_vec = reactions[product]
+
+    self.classifier.fit(user_vecs, reaction_vec)
+
+  def get_similar_users(self, user_vec):
+    return self.classifier.kneighbors(user_vec)
+
+  def predict_reaction(self, user_vec):
+    return self.classifier.predict_proba(user_vec)
 
 if __name__ == '__main__':
   app.run()
